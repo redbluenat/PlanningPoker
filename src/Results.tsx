ff62e51f39f80f518e37923abe838d0febe17e1b
Janina Harms <j.harms@lokalportal.de>
2019-10-14 08:16:41 +0200

    Orders the results

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Users } from './User';
-import { Card } from './Card';
+import React from "react";
+import { Users } from "./User";
+import { Card } from "./Card";
 
 interface IProps {
   ownId: string;
@@ -9,30 +9,46 @@ interface IProps {
 
 export const Results = ({ ownId, users }: IProps) => {
   const numberOfFinishedUsers = Object.keys(users).filter(
-    key => !!users[key] && !!users[key].value,
+    key => !!users[key] && !!users[key].value
   ).length;
   const totalNumberOfUsers = Object.keys(users).length;
   const disclose = numberOfFinishedUsers === totalNumberOfUsers;
 
   const getValue = (key: string) => {
-    if (users[key].value === undefined) {
-      return '_';
+    if (!users || !users[key] || users[key].value === undefined) {
+      return "_";
     }
 
     if (key === ownId) {
       return users[key].value;
     }
 
-    return disclose ? users[key].value : '?';
+    return disclose ? users[key].value : "?";
   };
+
+  const extendedUsers = Object.keys(users)
+    .map(key => ({ ...users[key], key }))
+    .sort((user1, user2) => {
+      if (!user1.value) return -1;
+
+      if (!user2.value) return 1;
+
+      const value1 = parseInt(user1.value);
+      if (isNaN(value1)) return 1;
+
+      const value2 = parseInt(user2.value);
+      if (isNaN(value2)) return -1;
+
+      return value1 - value2;
+    });
 
   return (
     <div>
-      {Object.keys(users).map(key => (
-        <Card key={key}>
+      {extendedUsers.map(extendedUser => (
+        <Card key={extendedUser.key}>
           <div>
-            <div style={{ padding: 16 }}>{users[key].name}</div>
-            <div style={{ fontSize: 80 }}>{getValue(key)}</div>
+            <div style={{ padding: 16 }}>{extendedUser.name}</div>
+            <div style={{ fontSize: 80 }}>{getValue(extendedUser.key)}</div>
           </div>
         </Card>
       ))}
