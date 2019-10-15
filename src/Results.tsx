f544a61f19977f84d8b2039b61008b9f608f9974
Janina Harms <j.harms@lokalportal.de>
2019-10-15 21:46:30 +0200

    Own Card is now highlighted

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Users } from './User';
-import { Card } from './Card';
+import React from "react";
+import { Users } from "./User";
+import { Card } from "./Card";
 
 interface IProps {
   ownId: string;
@@ -9,27 +9,30 @@ interface IProps {
 
 export const Results = ({ ownId, users }: IProps) => {
   const numberOfFinishedUsers = Object.keys(users).filter(
-    key => !!users[key] && !!users[key].value,
+    key => !!users[key] && !!users[key].value
   ).length;
   const totalNumberOfUsers = Object.keys(users).length;
   const disclose = numberOfFinishedUsers === totalNumberOfUsers;
+  const isOwnResult = (key: string) => {
+    return key === ownId;
+  };
 
   const getValue = (key: string) => {
     if (users[key].value === undefined) {
-      return '_';
+      return "_";
     }
 
-    if (key === ownId) {
+    if (isOwnResult(key)) {
       return users[key].value;
     }
 
-    return disclose ? users[key].value : '?';
+    return disclose ? users[key].value : "?";
   };
 
   return (
     <div>
       {Object.keys(users).map(key => (
-        <Card key={key}>
+        <Card key={key} color={isOwnResult(key) ? "#c3f7f7" : undefined}>
           <div>
             <div style={{ padding: 16 }}>{users[key].name}</div>
             <div style={{ fontSize: 80 }}>{getValue(key)}</div>
