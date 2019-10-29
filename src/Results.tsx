fe897c21f21c7914bf3f91a93058e519439f2aa2
Natalia Majkowska <natalia@brainsandbeards.com>
2019-10-29 18:30:39 +0100

    Add prettier config and scripts

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Users } from "./User";
-import { Card } from "./Card";
+import React from 'react';
+import { Users } from './User';
+import { Card } from './Card';
 
 interface IProps {
   ownId: string;
@@ -9,7 +9,7 @@ interface IProps {
 
 export const Results = ({ ownId, users }: IProps) => {
   const numberOfFinishedUsers = Object.keys(users).filter(
-    key => !!users[key] && !!users[key].value
+    key => !!users[key] && !!users[key].value,
   ).length;
   const totalNumberOfUsers = Object.keys(users).length;
   const disclose = numberOfFinishedUsers === totalNumberOfUsers;
@@ -19,14 +19,14 @@ export const Results = ({ ownId, users }: IProps) => {
 
   const getValue = (key: string) => {
     if (!users || !users[key] || users[key].value === undefined) {
-      return "_";
+      return '_';
     }
 
     if (isOwnResult(key)) {
       return users[key].value;
     }
 
-    return disclose ? users[key].value : "?";
+    return disclose ? users[key].value : '?';
   };
 
   const extendedUsers = Object.keys(users)
@@ -57,14 +57,14 @@ export const Results = ({ ownId, users }: IProps) => {
         return (
           <Card
             key={extendedUser.key}
-            color={isOwnResult(extendedUser.key) ? "#c3f7f7" : undefined}
+            color={isOwnResult(extendedUser.key) ? '#c3f7f7' : undefined}
           >
             <div>
               <div style={{ padding: 16 }}>{extendedUser.name}</div>
               <div
                 style={{
                   fontSize: 80,
-                  color: disclose && (isMin || isMax) ? "red" : "black"
+                  color: disclose && (isMin || isMax) ? 'red' : 'black',
                 }}
               >
                 {getValue(extendedUser.key)}
